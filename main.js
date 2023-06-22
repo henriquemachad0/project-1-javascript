e0395436c254e9e8227388cda27618ee5f314a63
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-06-22 16:00:22 -0300

    feat: add function celsiusToFahr

--- main.js
+++ main.js
@@ -9,11 +9,18 @@ document.querySelector("#close-nav-menu").addEventListener("click", function(){
 
 // Greeting section
 
+function celsiusToFahr(temperature){
+ let fahr = (temperature * 9/5) + 32
+ return fahr
+}
+
+celsiusToFahr(25)
+
 const greetingText = "Good morning!"
 const weatherCondition = "sunny"
 const userLocation = "New York"
 let temperature = 22.8673
-let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside.`
+let weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature.toFixed(1))}°F outside.`
 
 document.querySelector("#greeting").innerHTML = greetingText;
 document.querySelector("p#weather").innerHTML = weatherText;
