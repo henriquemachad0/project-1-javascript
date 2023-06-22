bdfcc7526e1f51a01fccd7e3b08cda7c18857046
henrique-machado-ziava <74209949+henriquemachad0@users.noreply.github.com>
2023-06-22 18:20:55 -0300

    fix: formatting the clock with padStart

--- main.js
+++ main.js
@@ -35,12 +35,10 @@ document.querySelector(".weather-group").addEventListener("click", function(even
     }
 })
 
-
-
 setInterval(function(){
     let localTime = new Date();
 
-    document.querySelector("span[data-time=hours]").textContent = localTime.getHours()
-    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes()
-    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds()
+    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2, "0");
+    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2, "0");
+    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2, "0");
 }, 1000)
